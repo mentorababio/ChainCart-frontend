6e2aa918c915b341726fd91764e8b9bfae255167
fido1hn <confidoekeanya@gmail.com>
2025-05-03 21:08:26 +0100

    fix: build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
- 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+// import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 // import { buffer } from 'stream/consumers'
 // https://vitejs.dev/config/
 export default defineConfig({
-//    server: {
-//      proxy: {
-//      // Target is your backend API
-//        '/api': {
-//            target: 'http://localhost:5000', 
-//            changeOrigin: true,
-            
-//           //  rewrite: (path) => path.replace(/^\/api/, ''),
-           
-//            configure: (proxy) => {
-//               proxy.on('error', (err) => {
-//                console.log('error', err);
-//               });
-//               proxy.on('proxyReq', (_, req) => {
-//                console.log('Request sent to target:', req.method, req.url);
-//               });
-//               proxy.on('proxyRes', (proxyRes, req) => {
-//                console.log('Response received from target:', proxyRes.statusCode, req.url);
-//               });
-//         },
-//      },
-//    },
-//  },   
-plugins: [react(),nodePolyfills({
-    include: ['buffer', 'util', 'stream']
-})],
+  //    server: {
+  //      proxy: {
+  //      // Target is your backend API
+  //        '/api': {
+  //            target: 'http://localhost:5000',
+  //            changeOrigin: true,
+
+  //           //  rewrite: (path) => path.replace(/^\/api/, ''),
+
+  //            configure: (proxy) => {
+  //               proxy.on('error', (err) => {
+  //                console.log('error', err);
+  //               });
+  //               proxy.on('proxyReq', (_, req) => {
+  //                console.log('Request sent to target:', req.method, req.url);
+  //               });
+  //               proxy.on('proxyRes', (proxyRes, req) => {
+  //                console.log('Response received from target:', proxyRes.statusCode, req.url);
+  //               });
+  //         },
+  //      },
+  //    },
+  //  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      buffer:'buffer'
+      buffer: "buffer",
     },
   },
-})
-
+});
